690285fc86555f81358b2cfb447e181c31a638e2
Thales <oliveiraethales@gmail.com>
2011-01-31 19:30:22 -0200

    Solution added

---
+++ GeoCache.Common/MongoRepository/MongoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeoCache.Contracts;
+
+namespace GeoCache.Common.MongoRepository
+{
+    public class MongoRepository : IRepository
+    {
+        public void Insert(Dictionary<string, object> document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Dictionary<string, object>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string query, Dictionary<string, object> updatingDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeoCache.Contracts/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoCache.Contracts
+{
+    public interface IRepository : IDisposable
+    {
+        /// <summary>
+        /// Insert a document in the data repository.
+        /// </summary>
+        /// <param name="document">
+        /// O documento
+        /// </param>
+        void Insert(Dictionary<string, object> document);
+
+        /// <summary>
+        /// Remove all the data from the repository.
+        /// </summary>
+        void RemoveAll();
+
+        /// <summary>
+        /// Gets all the documents from the repository.
+        /// </summary>
+        IEnumerable<Dictionary<string, object>> GetAll();
+
+        /// <summary>
+        /// Updates a document in the data repository.
+        /// </summary>
+        void Update(string query, Dictionary<string, object> updatingDocument);
+    }
+}
