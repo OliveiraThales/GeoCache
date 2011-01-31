583511c9d3454a4ca1abc490ba6c483a9b23c055
Thales <oliveiraethales@gmail.com>
2011-01-31 19:55:32 -0200

    added mongo repository implementation class

--- GeoCache.Common/MongoRepository/MongoRepository.cs
+++ GeoCache.Common/MongoRepository/MongoRepository.cs
@@ -3,34 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GeoCache.Contracts;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace GeoCache.Common.MongoRepository
 {
+    /// <summary>
+    /// Implementation class for Mongo data repository
+    /// </summary>
     public class MongoRepository : IRepository
     {
+        private MongoCollection collection;
+        private MongoDatabase database;
+        private MongoServer server;
+
+        public MongoRepository()
+        {
+            Connect();
+        }
+
+        private void Connect()
+        {
+            server = MongoServer.Create(string.Format("mongodb://localhost"));
+            database = server["GeoCache"];
+            collection = database["Features"];
+        }
+
         public void Insert(Dictionary<string, object> document)
         {
-            throw new NotImplementedException();
+            collection.Insert(document);
         }
 
         public void RemoveAll()
         {
-            throw new NotImplementedException();
+            collection.RemoveAll();
         }
 
         public IEnumerable<Dictionary<string, object>> GetAll()
         {
-            throw new NotImplementedException();
+            return collection.FindAllAs<Dictionary<string, object>>();
         }
 
-        public void Update(string query, Dictionary<string, object> updatingDocument)
+        public void Update(Dictionary<string, object> queryDocument, Dictionary<string, object> updatingDocument)
         {
-            throw new NotImplementedException();
+            var query = MongoDB.Driver.Builders.Query.Wrap(queryDocument);
+            var updating = MongoDB.Driver.Builders.Update.Wrap(updatingDocument);
+            collection.Update(query, updating);
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            server.Disconnect();
         }
     }
 }
