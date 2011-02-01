57da59b9e071dc21baa5d60f87e33a3f0003aed8
Thales <oliveiraethales@gmail.com>
2011-02-01 08:35:08 -0200

    Unit test

---
+++ GeoCache.Common.Test/MongoRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using GeoCache.Common.Repository;
+
+namespace GeoCache.Common.Test
+{
+    [TestFixture]
+    public class MongoRepositoryTest
+    {
+        private MongoRepository repository;
+
+        [SetUp]
+        public void Setup()
+        {
+            repository = new MongoRepository();
+        }
+
+        [Test]
+        public void MustInsertDocument()
+        {
+            var document = new Dictionary<string, object> {{"key", "value"}};
+            repository.Insert(document);
+
+            var documentFound = repository.FindOne();
+
+            Assert.AreEqual("value", documentFound["key"]);
+        }
+    }
+}
---
+++ GeoCache.Common/Repository/MongoRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using GeoCache.Contracts;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace GeoCache.Common.Repository
+{
+    /// <summary>
+    /// Implementation class for Mongo data repository
+    /// </summary>
+    public class MongoRepository : IRepository
+    {
+        private MongoCollection _collection;
+        private MongoDatabase _database;
+        private MongoServer _server;
+
+        public MongoRepository()
+        {
+            Connect();
+        }
+
+        private void Connect()
+        {
+            _server = MongoServer.Create(string.Format("mongodb://localhost"));
+            _database = _server["GeoCache"];
+            _collection = _database["Features"];
+        }
+
+        public Dictionary<string, object> FindOne()
+        {
+            return _collection.FindOneAs<Dictionary<string, object>>();
+        }
+
+        public void Insert(Dictionary<string, object> document)
+        {
+            var bsonDocument = new BsonDocument(document);
+            _collection.Insert(bsonDocument);
+        }
+
+        public void RemoveAll()
+        {
+            _collection.RemoveAll();
+        }
+
+        public IEnumerable<Dictionary<string, object>> GetAll()
+        {
+            return _collection.FindAllAs<Dictionary<string, object>>();
+        }
+
+        public void Update(Dictionary<string, object> queryDocument, Dictionary<string, object> updatingDocument)
+        {
+            var query = MongoDB.Driver.Builders.Query.Wrap(queryDocument);
+            var updating = MongoDB.Driver.Builders.Update.Wrap(updatingDocument);
+            _collection.Update(query, updating);
+        }
+
+        public void Dispose()
+        {
+            _server.Disconnect();
+        }
+    }
+}
