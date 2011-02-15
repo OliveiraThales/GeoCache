7fd0f0e535b3499be622ec86fc0774862f2b5bf9
Alaor Bianco <abianco@img.com.br>
2011-02-15 17:40:54 -0200

    Add cache implementation
    Signed-off-by: Alaor Bianco <abianco@img.com.br>

---
+++ GeoCache.Contracts/INoSqlRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoCache.Contracts
+{
+    public interface INoSqlRepository : IDisposable, IRepository
+    {
+        /// <summary>
+        /// Insert a document in the data repository.
+        /// </summary>
+        /// <param name="featureClassName">featureClassName</param>
+        /// <param name="geometry">
+        /// O documento
+        /// </param>
+        void Insert(string featureClassName, IGeometry geometry);
+
+        /// <summary>
+        /// Remove all the data from the repository.
+        /// </summary>
+        void RemoveAll(string featureClassName);
+    }
+}
---
+++ GeoCache.Contracts/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace GeoCache.Contracts
+{
+    /// <summary>
+    /// Generic geographic repository
+    /// </summary>
+    public interface IRepository
+    {
+        /// <summary>
+        /// Get all geometries from a featureClass
+        /// </summary>
+        /// <param name="featureClassName">FeatureClass name</param>
+        /// <returns>Enumerable of geometries</returns>
+        IEnumerable<IGeometry> GetAll(string featureClassName);
+
+        /// <summary>
+        /// Get all geometries inside the envelope from a featureClass
+        /// </summary>
+        /// <param name="featureClassName">FeatureClass name</param>
+        /// <param name="envelope">Envelope</param>
+        /// <returns>Enumerable of geometries</returns>
+        IEnumerable<IGeometry> GetByEnvelope(string featureClassName, IEnvelope envelope);
+
+        /// <summary>
+        /// Get full extent of featureClass
+        /// </summary>
+        /// <param name="featureClassName">FeatureClass name</param>
+        /// <returns>Envelope</returns>
+        IEnvelope GetFullExtent(string featureClassName);
+    }
+}
